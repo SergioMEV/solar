04d2a45740d46d0e2cecd874934e1aedf57373ae
fjadbeth <74471478+fjadbeth@users.noreply.github.com>
2022-12-13 15:35:52 -0600

    Update query_util.h

--- query_util.h
+++ query_util.h
@@ -3,4 +3,4 @@
 #define SEPERATOR "\n"
 
 // Concatenates all inputs to one string variable.
-char* string_concatenate(int fd, char* user_name, char* line_num, char* action, char* message);
+char* string_concatenate(char* user_name, char* line_num, char* action, char* message);
