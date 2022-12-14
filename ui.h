48cdf0523472905c5d63c790c0cfec96bfa25572
SergioMEV <sergiomarteloe@gmail.com>
2022-12-14 20:19:23 +0000

    UI header file

---
+++ ui.h
@@ -0,0 +1,18 @@
+// MISC FUNCTIONS
+static char *trim_whitespaces(char *str);
+int print_text(int min_line, int display_max_y, file_content_t *file_content);
+void modify_action_display(char action, int line_index);
+
+// SETUP FUNCTIONS
+void screensetup(void);
+void text_box_setup();
+void instructions_setup();
+
+// DRIVERS
+
+bool text_box_driver(file_content_t *file_content);
+void line_selection_driver(file_content_t *file_content, int ch, int max_line);
+bool display_driver(file_content_t *file_content);
+
+// MAIN THREAD FUNCTION
+void *ui_thread_handler(void *args);
